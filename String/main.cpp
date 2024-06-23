14c27c1d09f35fad5fae74f8f15a82279e28d272
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-06-23 10:27:18 +0500

    Added a check = to Indexing operators Home Work DONE

--- String/main.cpp
+++ String/main.cpp
@@ -8,8 +8,8 @@ using std::endl;
 
 class String
 {
-	int	size;
-	char* str;
+	int	size;  //Размер строки в байтах
+	char* str; //Указатель на строку в динамической памяти
 public:
 	int get_size()const
 	{
@@ -24,7 +24,7 @@ public:
 		return str;
 	}
 	//			Constructors:
-	String(int size = 80)
+	explicit String(int size = 80)
 	{
 		this->size = size;
 		this->str = new char[size] {};
@@ -45,24 +45,49 @@ public:
 		delete[] this->str;
 		cout << "Destructor:\t\t" << this << endl;
 	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	//				Operators:
 	String& operator=(const String& other)
 	{
+		if (this == &other)return *this;
 		delete[] str;
 		this->size = other.size;
 		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
 		{
-			for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+			this->str[i] = other.str[i];
 		}
 		cout << "CopyAssignment:\t\t" << this << endl;
 		return *this;
 	}
+	//				Indexing operators
+	char& operator[](int index)
+	{
+		return str[index];
+	}
+	const char& operator[](int index) const
+	{
+		return str[index];
+	}
 	//				Methods:
 	void print()const
 	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
+		cout << "Obj;\t\t\t" << this << endl;
+		cout << "Size:\t\t\t" << size << endl;
+		cout << "Addr:\t\t\t" << &str << endl;
+		cout << "Str:\t\t\t" << str << endl;
+		cout << delimiter << endl;
 	}
+	
 };
 
 std::ostream& operator<<(std::ostream& os, const String& obj)
@@ -72,13 +97,16 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 String operator+(const String& left, const String& right)
 {
 	String buffer(left.get_size() + right.get_size() - 1);
+	buffer.print();
 	for (int i = 0; i < left.get_size(); i++)
 	{
-		buffer.get_str()[i] = left.get_str()[i];
+		buffer[i] = left[i];
+		//buffer.get_str()[i] = left.get_str()[i]; 
 	}
 	for (int i = 0; i < right.get_size(); i++)
 	{
-		buffer.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+		buffer[i + left.get_size() - 1] = right[i];
+		//buffer.get_str()[i + left.get_size() - 1] = right.get_str()[i]; 
 	}
 	return buffer;
 }
@@ -86,25 +114,27 @@ String operator+(const String& left, const String& right)
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << delimiter << endl;
+
 	String str1;
 	str1.print();
 
 	String str2(8);
 	str2.print();
-	cout << delimiter << endl;
 
 	String str3 = "Hello";
-	cout << str3 << endl;
+	//cout << str3 << endl;
+	str3.print();
+
+	str3 = str3;
 
 	String str4 = "World";
-	cout << str4 << endl;
+	//cout << str4 << endl;
+	str4.print();
 
-	cout << delimiter << endl;
 	//String str5 = str3 + " " + str4;
 	String str5;
 	str5 = str3 + " " + str4;
-
+	
 	cout << delimiter << endl;
 	cout << str5 << endl;
 	cout << delimiter << endl;
