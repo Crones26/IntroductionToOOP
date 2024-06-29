f1b7f37ebbfa3192218485c0345d89b8d651be36
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-06-29 13:05:12 +0500

    Constructor Delegation DONE

--- String/main.cpp
+++ String/main.cpp
@@ -30,7 +30,7 @@ public:
 		//this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char str[]) :size(strlen(str)+1), str(new char[size] {})
+	String(const char str[]) :String(strlen(str) + 1)
 	{
 		//this->size = strlen(str) + 1;
 		//this->str = new char[size] {};
@@ -40,7 +40,7 @@ public:
 		}
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other) :size(other.size), str(new char[size] {})
+	String(const String& other) :String(other.str)
 	{
 		//this->size = other.size;
 		//this->str = new char[size] {};
